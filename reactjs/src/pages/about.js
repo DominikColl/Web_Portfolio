332c03e00f629167874012b4557d94704ab8a6e2
DominikColl <DAColl@student.fullsail.edu>
2020-05-12 18:05:13 -0700

    Added react project and creatin react components added routing as well

---
+++ reactjs/src/pages/about.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Landing extends Component {
+    state = {}
+    render() {
+        return (<h1>About page</h1>);
+    }
+}
+
+export default Landing;
---
+++ reactjs/src/pages/nav.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Nav extends Component {
+    state = {}
+    render() {
+        return (<div><ul><li>Home</li><li>About</li><li>Contact</li></ul></div>);
+    }
+}
+
+export default Nav;
