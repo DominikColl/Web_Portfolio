399b116d55e4c322f457dc0cde1077b8a089de68
DominikColl <DAColl@student.fullsail.edu>
2020-05-19 13:36:18 -0700

    Adding more content and design changes to landing page

--- reactjs/src/pages/landing.js
+++ reactjs/src/pages/landing.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import '../styles.css'
+import i from '../img/aboutPic.png'
+import livefmPic from '../img/liveFmAPI.png'
 class Landing extends Component {
     state = {}
     render() {
@@ -11,10 +13,10 @@ class Landing extends Component {
                 <section id='projectSection'>
                     <h2>Projects</h2>
                     <ul id='projectList'>
-                        <li><p>Quiz App</p></li>
-                        <li><p>LiveFm API</p></li>
-                        <li><p>Task Manager</p></li>
-                        <li><p>Twitter Clone</p></li>
+                        <li id='quizApp'><div><p>Quiz App</p></div></li>
+                        <li id='livefm'><p>LiveFm API</p></li>
+                        <li id='task'><p>Task Manager</p></li>
+                        <li id='twitClone'><p>Twitter Clone</p></li>
                     </ul>
                 </section>
                 <section id='blogSection'>
@@ -25,6 +27,24 @@ class Landing extends Component {
                         <li><p>Temp</p></li>
                         <li><p>Temp</p></li>
                     </ul>
+                </section>
+                <section id='skillsSection'>
+                    <p>Skills Include</p>
+                    <ul id='skillsList'>
+                        <li><h2>Javascript</h2></li>
+                        <li><h2>ReactJs</h2></li>
+                        <li><h2>HTML</h2></li>
+                        <li><h2>CSS/SASS</h2></li>
+                        <li><h2>Web Applications</h2></li>
+                        <li><h2>UI/UX</h2></li>
+                    </ul>
+                </section>
+                <section id='about'>
+                    <h2>About me Dominik Coll</h2>
+
+                    <img id='aboutPic' src={i} alt='Dominik Coll' />
+
+                    <p>This is about me Dominik Coll thanks for reading</p>
                 </section>
             </Fragment>
 
