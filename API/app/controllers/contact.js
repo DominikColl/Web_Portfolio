7f9d1afa5aefcf05fe24351ada45e7a76f055904
DominikColl <DAColl@student.fullsail.edu>
2020-05-27 14:21:22 -0700

    Updating routes for contact

--- API/app/controllers/contact.js
+++ API/app/controllers/contact.js
@@ -4,5 +4,10 @@ exports.submitContact = async (req, res) => {
     console.log(req.body)
     const { name, email, reason } = req.body;
     let ret = await Contact.create({ name, email, reason })
-    res.json(ret)
+    res.json({ ret })
+}
+exports.deleteContact = async (req, res) => {
+    const id = req.params.id
+    let ret = await Contact.destroy({ where: { id } })
+    res.json({ ret })
 }
--- API/app/routes/contact.js
+++ API/app/routes/contact.js
@@ -3,5 +3,6 @@ const contactController = require('../controllers/contact')
 
 
 router.post('/contact', contactController.submitContact)
+router.get('/delete/:id', contactController.deleteContact)
 
 module.exports = router;
